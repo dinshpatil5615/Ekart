d6711ae41c0854fa92e060ba9575e346299b649b
Dinesh Patil <126959178+dinshpatil5615@users.noreply.github.com>
2026-04-23 17:52:21 +0530

    Update SpringSecurityConfig.java

--- src/main/java/com/reljicd/config/SpringSecurityConfig.java
+++ src/main/java/com/reljicd/config/SpringSecurityConfig.java
@@ -1,5 +1,4 @@
 package com.reljicd.config;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
@@ -10,9 +9,7 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.access.AccessDeniedHandler;
-
 import javax.sql.DataSource;
-
 /**
  * Spring Security Configuration
  * http://docs.spring.io/spring-boot/docs/current/reference/html/howto-security.html
@@ -22,29 +19,21 @@ import javax.sql.DataSource;
  */
 @Configuration
 public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
-
     private final AccessDeniedHandler accessDeniedHandler;
-
     final DataSource dataSource;
-
     @Value("${spring.admin.username}")
     private String adminUsername;
-
     @Value("${spring.admin.username}")
     private String adminPassword;
-
     @Value("${spring.queries.users-query}")
     private String usersQuery;
-
     @Value("${spring.queries.roles-query}")
     private String rolesQuery;
-
     @Autowired
     public SpringSecurityConfig(AccessDeniedHandler accessDeniedHandler, DataSource dataSource) {
         this.accessDeniedHandler = accessDeniedHandler;
         this.dataSource = dataSource;
     }
-
     /**
      * HTTPSecurity configurer
      * - roles ADMIN allow to access /admin/**
@@ -55,9 +44,9 @@ public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
      */
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-
         http.csrf().disable()
                 .authorizeRequests()
+                .antMatchers("/actuator/**").permitAll() // Allow Prometheus scraping
                 .antMatchers("/home", "/registration", "/error", "/h2-console/**").permitAll()
                 .anyRequest().authenticated()
                 .and()
@@ -73,14 +62,11 @@ public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
                 // Fix for H2 console
                 .and().headers().frameOptions().disable();
     }
-
-
     /**
      * Authentication details
      */
     @Autowired
     public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
-
         // Database authentication
         auth.
                 jdbcAuthentication()
@@ -88,12 +74,10 @@ public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
                 .authoritiesByUsernameQuery(rolesQuery)
                 .dataSource(dataSource)
                 .passwordEncoder(passwordEncoder());
-
         // In memory authentication
         auth.inMemoryAuthentication()
                 .withUser(adminUsername).password(adminPassword).roles("ADMIN");
     }
-
     /**
      * Configure and return BCrypt password encoder
      */
@@ -101,5 +85,4 @@ public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
-
 }
